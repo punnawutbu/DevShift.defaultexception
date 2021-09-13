21eac74420a0d4d9632d374822343d3584d03de4
traithep.jun <traithep@nioachievers.com>
2021-09-13 14:35:23 +0700

    update

--- NIO.DefaultException/DefaultExtension.cs
+++ NIO.DefaultException/DefaultExtension.cs
@@ -56,7 +56,9 @@ public static class DefaultExtension
                             else
                                 response.StatusCode = (Int32)HttpStatusCode.InternalServerError;                                    
 
-                            exResponse.Message = $"{handlerFeature.Error.Message} {handlerFeature.Error.TargetSite.DeclaringType.FullName}.{new StackTrace(handlerFeature.Error).GetFrame(0).GetMethod().Name}";
+                            // exResponse.Exception = $"{handlerFeature.Error.TargetSite.DeclaringType.FullName}.{new StackTrace(handlerFeature.Error).GetFrame(0).GetMethod().Name}";
+                            exResponse.Exception = "test custome exception";
+                            exResponse.Message = handlerFeature.Error.Message;
                             exResponse.Path = ((ExceptionHandlerFeature)handlerFeature).Path;
                             exResponse.Status = response.StatusCode;
                             
